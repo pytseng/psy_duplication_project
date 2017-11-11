13d860147cfc6c350abe87d166854416d68e889c
ctcaulfield <ctcaulfield@gmail.com>
2017-11-11 14:48:24 -0500

    Updated code

--- replicate.js
+++ replicate.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 		if(counter1>1){
 			$('#ad01').show();
 			$('#ball06').hide();
-		}			  
+		}
 	})
 	$('#cat02').on('show.bs.collapse', function () {
 	  $('#cat01').collapse('hide');
@@ -19,9 +19,9 @@ $(document).ready(function(){
 		counter2 +=1;
 		if(counter2>1){
 			$('#ad02').show();
-		}		
+		}
 	})
-	
+
 	$('#cat03').on('show.bs.collapse', function () {
 	  $('#cat01').collapse('hide');
 	  $('#cat02').collapse('hide');
@@ -29,6 +29,22 @@ $(document).ready(function(){
 		counter3 +=1;
 		if(counter3>1){
 			$('#ad03').show();
-		}	  
-	})	
+		}
+	})
 });
+
+
+$("#countdown").countdown360({
+  radius      : 60.5,
+  seconds     : 5,
+  strokeWidth : 15,
+  fillStyle   : '#0276FD',
+  strokeStyle : '#003F87',
+  fontSize    : 50,
+  fontColor   : '#FFFFFF',
+  autostart: false,
+  onComplete  : function () {
+		console.log('completed')
+		// window.location.href = "https://docs.google.com/forms/u/0/?tgif=d"
+	}
+}).start()
