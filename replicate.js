75e27cb3a676f1e5b1118f6316b9d495ec9529a2
ctcaulfield <ctcaulfield@gmail.com>
2017-11-12 03:03:02 -0500

    v2 out

--- replicate.js
+++ replicate.js
@@ -1,6 +1,8 @@
 var counter1 = 0;
 var counter2 = 0;
 var counter3 = 0;
+
+
 $(document).ready(function(){
 	$('#cat01').on('show.bs.collapse', function () {
 	  $('#cat02').collapse('hide');
@@ -33,7 +35,33 @@ $(document).ready(function(){
 	})
 });
 
+// order of the items
+var itemOrder = ['basketball','football','baseball','muscle','arnold','yoga']
 
+// order of the items - this is for the hard one - will be edited
+var hard_itemOrder = ['basketball','football','baseball','muscle','arnold','yoga']
+
+//what position is the user in their cart
+var position = 0
+//addItem is called when user selects on the correct item
+//parameter: value - for name of item selected by user
+function addItem(value){
+	//is the value they select in the
+	//correct order position as the itemOrder array
+	if(itemOrder.indexOf(value)==position){
+    //item is in array and in correct order!
+		position++;
+		document.getElementById("cart").innerHTML = position;
+		$('#cat01').collapse('hide');
+		$('#cat02').collapse('hide');
+		$('#cat03').collapse('hide');
+    //if they have all the items - take them to survey - they are done!
+		if(position>=itemOrder.length){
+			window.location.href = "https://docs.google.com/forms/u/0/?tgif=d"
+		}
+	}
+}
+//countdown timer for user - when done - redirect to the form!
 $("#countdown").countdown360({
   radius      : 60.5,
   seconds     : 5,
