76d81a6214533cc9cca93c4f94cea4945e63a70d
ctcaulfield <ctcaulfield@gmail.com>
2017-11-13 15:13:28 -0500

    Changes!

--- eas/replicate.js
+++ eas/replicate.js
@@ -1,37 +1,16 @@
-var counter1 = 0;
-var counter2 = 0;
-var counter3 = 0;
-
-
 $(document).ready(function(){
 	$('#cat01').on('show.bs.collapse', function () {
 	  $('#cat02').collapse('hide');
 	  $('#cat03').collapse('hide');
-		console.log("ddd");
-		counter1 +=1;
-		if(counter1>1){
-			$('#ad01').show();
-			$('#ball06').hide();
-		}
 	})
 	$('#cat02').on('show.bs.collapse', function () {
 	  $('#cat01').collapse('hide');
 	  $('#cat03').collapse('hide');
-	  		console.log("ddd");
-		counter2 +=1;
-		if(counter2>1){
-			$('#ad02').show();
-		}
 	})
 
 	$('#cat03').on('show.bs.collapse', function () {
 	  $('#cat01').collapse('hide');
 	  $('#cat02').collapse('hide');
-		console.log("ddd");
-		counter3 +=1;
-		if(counter3>1){
-			$('#ad03').show();
-		}
 	})
 });
 
